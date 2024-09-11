bc5929e6ee348765ec0b3c761ce57e2e0bff5131
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-09-11 20:25:47 +0100

    ✨ added chains

--- src/chain.ts
+++ src/chain.ts
@@ -14,3 +14,33 @@ export const EDUChainTestnet = defineChain({
     },
   },
 });
+
+export const BaseTestnet = defineChain({
+  id: 84532,
+  name: "Base Sepolia Testnet",
+  nativeCurrency: { name: "Ethereum", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://sepolia.base.org"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Base Sepolia Testnet Explorer",
+      url: "https://sepolia.basescan.org",
+    },
+  },
+});
+
+export const LiskTestnet = defineChain({
+  id: 4202,
+  name: "Lisk Sepolia Testnet",
+  nativeCurrency: { name: "Ethereum", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://rpc.sepolia-api.lisk.com"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Lisk Sepolia Testnet Explorer",
+      url: "https://sepolia-blockscout.lisk.com",
+    },
+  },
+});
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { arbitrum } from "wagmi/chains";
-import { EDUChainTestnet } from "./chain";
+import { BaseTestnet, EDUChainTestnet, LiskTestnet } from "./chain";
 
 export const config = getDefaultConfig({
   appName: "EduVR",
   projectId: "d4a6f4edae5c0b3b2352b1e169c92123",
-  chains: [
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-      ? [EDUChainTestnet]
-      : []),
-  ],
+  chains: [BaseTestnet, LiskTestnet, EDUChainTestnet],
   ssr: true,
 });
