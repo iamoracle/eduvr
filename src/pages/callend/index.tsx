792cf07ff7ce716ddebe6528621fefdb62a2e6af
olagunjuea1 <olagunjuea1@gmail.com>
2024-09-11 21:18:52 +0100

    🐛 fix bug

--- src/pages/callend/index.tsx
+++ src/pages/callend/index.tsx
@@ -19,7 +19,7 @@ const Callend = () => {
       <div className="w-full h-screen min-h-screen flex flex-col items-center justify-center overflow-hidden _Container p-6">
         <div className="flex flex-col md:flex-row justify-center gap-y-6 md:gap-y-0 gap-x-8 w-full py-20">
           <div className="pr-10 max-w-md text-center">
-            <h2 className="text-4xl font-bold">ClassRoom Ended</h2>
+            <h2 className="text-4xl font-bold">Meeting Ended</h2>
             <p className="mt-4 text-base">
               Select an available seat to fully immerse yourself in the class
               experience.
