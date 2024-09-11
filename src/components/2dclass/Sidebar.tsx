c9a817b493d2bb68d738a65c6ffb3347f9c3f13e
olagunjuea1 <olagunjuea1@gmail.com>
2024-09-11 12:27:31 +0100

    🐛 fix sidebar 2d added, mic and video icon

--- src/components/2dclass/Sidebar.tsx
+++ src/components/2dclass/Sidebar.tsx
@@ -48,11 +48,13 @@ const Sidebar: React.FC<Props> = ({ toggleSideBarState, sideBarState }) => {
         const participantCount = participants.length;
         settotalparticipants(participantCount);
 
-        const updatedParticipants = participants.map((participant:any, index:any) => ({
-          ...participant,
-          svg: toSvg(`${participant.position}`, 25), // Assign SVG, use index or other identifier
-          backgroundColor: getRandomPastelColor() // Assign random pastel color
-        }));
+        const updatedParticipants = participants.map(
+          (participant: any, index: any) => ({
+            ...participant,
+            svg: toSvg(`${participant.position}`, 25), // Assign SVG, use index or other identifier
+            backgroundColor: getRandomPastelColor(), // Assign random pastel color
+          })
+        );
         setparticipants(updatedParticipants);
       } catch (error) {
         console.error("Failed to fetch participants:", error);
@@ -86,7 +88,7 @@ const Sidebar: React.FC<Props> = ({ toggleSideBarState, sideBarState }) => {
         </h2>
       </div>
       <div className="flex flex-col gap-y-4 flex-nowrap items-start pt-2 overflow-y-auto h-[calc(100vh-80px)] max-h-[calc(100vh-80px)]">
-        {participants.map((data : any, index) => {
+        {participants.map((data: any, index) => {
           return (
             <div className="flex flex-row items-center w-full" key={index}>
               <div
@@ -95,18 +97,38 @@ const Sidebar: React.FC<Props> = ({ toggleSideBarState, sideBarState }) => {
               >
                 {parse(data.svg)}
               </div>
-              <p
-                className={`text-sm ml-3 flex flex-col ${
-                  sideBarState ? "hidden" : "block"
-                }`}
-              >
-                <span className="text-slate-700 dark:text-slate-300 font-medium">
-                  Anonymouns {data.position}
-                </span>
-                <span className="text-xs text-slate-400 dark:text-slate-500">
-                  Student
-                </span>
-              </p>
+              <div className="flex flex-row justify-between flex-1 gap-x-3 items-center">
+                <p
+                  className={`text-sm flex flex-col ${
+                    sideBarState ? "hidden" : "block ml-3"
+                  }`}
+                >
+                  <span className="text-slate-700 dark:text-slate-300 font-medium">
+                    Anonymous {data.position}
+                  </span>
+                </p>
+                <p className={`flex flex-row gap-x-2 ${sideBarState ? "hidden" : "block"}`}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-[1.12rem] h-w-[1.12rem] text-slate-400 dark:text-slate-500"
+                    viewBox="0 0 24 24"
+                    style={{ msFilter: "" }}
+                    fill="currentColor"
+                  >
+                    <path d="M21.707 20.293l-3.388-3.388A7.942 7.942 0 0020 12.021h-2a5.95 5.95 0 01-1.109 3.456l-1.452-1.452c.348-.591.561-1.27.561-2.004v-6C16 3.804 14.215 2 12.021 2c-.07 0-.14.009-.209.025A4.005 4.005 0 008 6.021v.565L3.707 2.293 2.293 3.707l18 18 1.414-1.414zM10 6.021c0-1.103.897-2 2-2a.918.918 0 00.164-.015C13.188 4.08 14 4.956 14 6.021v6c0 .172-.029.335-.071.494L10 8.586V6.021zm-4 6H4c0 4.072 3.06 7.436 7 7.931v2.069h2v-2.07a7.993 7.993 0 002.218-.611l-1.558-1.558a5.979 5.979 0 01-1.66.239c-3.309 0-6-2.692-6-6z"></path>
+                    <path d="M8.011 12.132a3.993 3.993 0 003.877 3.877l-3.877-3.877z"></path>
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-[1.12rem] h-w-[1.12rem] text-slate-400 dark:text-slate-500"
+                    viewBox="0 0 24 24"
+                    style={{ msFilter: "" }}
+                    fill="currentColor"
+                  >
+                    <path d="M18 7c0-1.103-.897-2-2-2H6.414L3.707 2.293 2.293 3.707l18 18 1.414-1.414L18 16.586v-2.919L22 17V7l-4 3.333V7zm-2 7.586L8.414 7H16v7.586zM4 19h10.879l-2-2H4V8.121L2.145 6.265A1.977 1.977 0 002 7v10c0 1.103.897 2 2 2z"></path>
+                  </svg>
+                </p>
+              </div>
             </div>
           );
         })}
